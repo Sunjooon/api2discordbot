9c411fc2af360f177700d4236c5740231f2253d4
johnnythors <johnnythors@gmail.com>
2023-01-03 01:33:31 +0000

    promises fully added, had to add urlencoded

--- src/app.js
+++ src/app.js
@@ -10,10 +10,11 @@ const api = require('./api');
 
 const app = express();
 
-app.use(morgan('dev'));
-app.use(helmet());
+// app.use(morgan('dev'));
+// app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended : true}))
 
 app.get('/', (req, res) => {
   res.json({
