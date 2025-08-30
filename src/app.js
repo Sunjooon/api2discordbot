aef63fdecdff0b85958172cff3b91a56509925a1
Sunjooon <109410146+Sunjooon@users.noreply.github.com>
2025-08-29 17:54:15 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
+// /src/app.js
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
-
 require('dotenv').config();
-
 const middlewares = require('./middlewares');
-const api = require('./api');
+// const api = require('./api');  // Comment this out or remove it
 
 const app = express();
 
@@ -18,11 +17,19 @@ app.use(express.urlencoded({extended : true}))
 
 app.get('/', (req, res) => {
   res.json({
-    message: 'index'
+    message: 'Discord Bot API'
   });
 });
 
-app.use('/api/v1', api);
+// Simple Discord endpoint directly here
+app.post('/api/v1/sendmessage', async (req, res) => {
+  // This will be handled by your Discord bot
+  res.json({
+    message: 'This endpoint needs Discord bot integration'
+  });
+});
+
+// app.use('/api/v1', api);  // Comment this out or remove it
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
